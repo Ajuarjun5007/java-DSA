7937a4c52444fba25b9606a28c41507f62ea1fec
Ajuarjun5007 <115939598+Ajuarjun5007@users.noreply.github.com>
2023-04-27 13:29:10 +0530

    Add files via upload

---
+++ movezeros283.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+public class movezeros283 {
+    public static void main(String[] args) {
+        int[] nums={0,1,0,12,5,0,3,0,0,9};
+
+          int[] ans=movezeros(nums);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(nums[i]+" ");
+        }
+
+    }
+
+    public static int[] movezeros(int[] nums) {
+        int boundary=0;
+        for(int i = 0; i<nums.length;i++) {
+
+            if(nums[i] != 0) {
+                int temp = nums[boundary];
+                nums[boundary]=nums[i];
+                nums[i]=temp;
+
+                boundary++;
+
+            }
+        }
+        return nums;
+    }
+}
