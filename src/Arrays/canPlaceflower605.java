9228efbaee136b72b4c6b961ac6309e7b63dbdc9
AjuArjun <arjunjeya9475@gmail.com>
2023-05-13 14:22:21 +0530

    13/05

---
+++ src/Arrays/canPlaceflower605.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class canPlaceflower605 {
+    public static void main(String[] args) {
+        int[] arr={1,0,1,0,1,0,0,0};
+        int n=1;
+        boolean ans =canPlaceFlowers(arr,n);
+        System.out.println(ans);
+    }
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int len=flowerbed.length;
+        int count=0;
+        boolean ans ;
+        if (n == 0)
+            return true;
+
+        for (int i = 0; i < flowerbed.length; ++i)
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) &&
+                    (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                if (--n == 0)
+                    return true;
+            }
+
+        return false;
+    }
+}
